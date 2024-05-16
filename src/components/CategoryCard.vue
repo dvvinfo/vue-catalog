4825087f4e7cb51faa7354c3498237ea0e5bc18b
dvvinfo <dvvinfo@mail.ru>
2024-05-16 15:27:05 +0300

    first commit

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="category-card" :style="{ backgroundColor: category.text_color }">
+    <h3 class="category-card__title">{{ category.name }}</h3>
+    <div class="category-card__image">
+      <img :src="category.image" :alt="category.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryCard',
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.category-card {
+  width: 271px;
+  height: 152px;
+  padding: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+   <h1>kk</h1>
+  </main>
+</template>
